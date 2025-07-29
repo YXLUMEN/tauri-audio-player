c60d25b52c40c72f14181f259f5efcc1f7da8b1f
YXLUMEN <lumengrace@outlook.com>
2025-07-29 19:40:01 +0800

    add 软件图标;

--- src/js/render/player.ts
+++ src/js/render/player.ts
@@ -418,7 +418,7 @@ document.getElementById('title-bar-close')?.addEventListener('click', async () =
     await appWindow.close()
 });
 
-window.addEventListener('beforeunload', savePlayingQueue);
+// window.addEventListener('beforeunload', savePlayingQueue);
 
 
 function initApp() {
